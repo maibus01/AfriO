1b8120b1efa234d2a02933b9b7177fcc12e83a0b
Mahmud <imahmud609@gmail.com>
2026-05-05 16:46:55 +0300

    Update API baseURL to Render link

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://afrio-api.onrender.com/api",
 });
-
 // Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
