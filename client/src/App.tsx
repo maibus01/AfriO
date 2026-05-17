28d4b85c42f55b9ecea5d8cf60749432a0a37cf5
maibus01 <imahmud609@gmail.com>
2026-05-17 17:36:23 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
             element={<BusinessDashboard />}
           />
 
-<Route path="/order-hub" element={<OrderHub />} />
+<Route path="/orders-hub" element={<OrderHub />} />
           <Route path="/chat" element={<ChatPage />} />
 
 
