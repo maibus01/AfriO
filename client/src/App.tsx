7a1a5b4dcde03a005b0dc8c63bcf50d6c6149a2c
maibus01 <imahmud609@gmail.com>
2026-05-19 19:11:04 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import ButtonBar from "./components/ButtonBar";
 import HomePage from "./pages/Home";
 import AuthPage from "./auth/AuthPage";
 import { CartProvider } from "./context/CartContext";
@@ -30,7 +29,6 @@ function App() {
     <CartProvider>
       <Router>
         <Navbar />
-        <ButtonBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/auth" element={<AuthPage />} />
