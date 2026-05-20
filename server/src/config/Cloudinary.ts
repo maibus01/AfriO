701e67c37d3c1f3dd95409e8b0ca72473d0b79bd
Mahmud <imahmud609@gmail.com>
2026-05-21 01:01:00 +0300

    claudly name change

--- server/src/config/Cloudinary.ts
+++ server/src/config/Cloudinary.ts
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "LuxeeHub",
+  cloud_name: "dvovlsvgp",
   api_key: "233243848937431",
   api_secret: "uD-79L8pWUDGNeMK4yuL8RlHOP8",
 });
