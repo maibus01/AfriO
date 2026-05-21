a9abefa02b586fe850ec795f051d23337c661aa6
Mahmud <imahmud609@gmail.com>
2026-05-21 08:05:47 +0300

    logo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { App as CapApp } from '@capacitor/app'
+
+// 🔥 Back button handling
+CapApp.addListener('backButton', () => {
+  if (window.history.length > 1) {
+    window.history.back()
+  } else {
+    const exit = confirm("Do you want to exit Luxee?")
+    if (exit) {
+      CapApp.exitApp()
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
