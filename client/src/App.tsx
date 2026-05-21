7a74455087544525c078561b936277320f673ebc
Mahmud <imahmud609@gmail.com>
2026-05-21 22:57:53 +0300

    app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,80 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ButtonBar from "./components/ButtonBar";
+// import HomePage from "./pages/Home";
+// import AuthPage from "./auth/AuthPage";
+// import { CartProvider } from "./context/CartContext";
+// import CartPage from "./pages/CartPage";
+// import Profile from "./pages/Profile";
+// import Business from "./pages/Business";
+// import Admin from "./pages/Admin"
+// import BusinessDashboard from "./pages/BusinessDashboard";
+// import Product from "./pages/Product";
+// import Style from "./pages/Style";
+// import ProductDetails from "./pages/ProductDetails";
+// import StyleDetails from "./pages/StyleDetails";
+// import PublicSeller from "./pages/PublicSeller";
+// import ChatPage from "./pages/ChatPage";
+// import OrderPage from "./pages/Order";
+// import AdminAccounts from "./pages/AdminAccouts";
+// import AdminOrders from "./pages/AdminOrders";
+// import AdminRequests from "./pages/AdminStyleRequest";
+// import RequestPaymentPage from "./pages/RequestPaymentPage";
+
+// import OrderHub from "./pages/OrderHub";
+// import Shops from "./pages/Shops";
+
+
+// function App() {
+//   return (
+//     <CartProvider>
+//       <Router>
+//         <ButtonBar />
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/auth" element={<AuthPage />} />
+//           <Route path="/cart" element={<CartPage />} />
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/business" element={<Business />} />
+//           <Route path="/admin" element={<Admin />} />
+//           {/* <Route path="/business/dashboard" element={<BusinessDashboard />} /> */}
+//           {/* 🏪 VENDOR DASHBOARD */}
+//           <Route path="/vendor/:id" element={<Product />} />
+//           <Route path="/tailor/:id" element={<Style />} />
+
+//           <Route path="/product/:id" element={<ProductDetails />} />
+//           <Route path="/style/:id" element={<StyleDetails />} />
+
+//           <Route
+//             path="/dashboard/business/:id"
+//             element={<BusinessDashboard />}
+//           />
+
+// <Route path="/orders-hub" element={<OrderHub />} />
+
+// <Route path="/shop" element={<Shops />} />
+
+//           <Route path="/chat" element={<ChatPage />} />
+
+
+//           <Route path="/business/:businessId/public" element={<PublicSeller />} />
+
+//           <Route path="/order" element={<OrderPage />} />
+//           <Route path="/admin/accounts" element={<AdminAccounts />} />
+
+
+//           <Route path="/admin/orders" element={<AdminOrders />} />
+//           <Route path="/admin/styles-request" element={<AdminRequests />} />
+//           <Route path="/request-payment" element={<RequestPaymentPage />} />
+
+//         </Routes>
+//       </Router>
+//     </CartProvider>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ButtonBar from "./components/ButtonBar";
 import HomePage from "./pages/Home";
@@ -6,7 +83,7 @@ import { CartProvider } from "./context/CartContext";
 import CartPage from "./pages/CartPage";
 import Profile from "./pages/Profile";
 import Business from "./pages/Business";
-import Admin from "./pages/Admin"
+import Admin from "./pages/Admin";
 import BusinessDashboard from "./pages/BusinessDashboard";
 import Product from "./pages/Product";
 import Style from "./pages/Style";
@@ -19,65 +96,67 @@ import AdminAccounts from "./pages/AdminAccouts";
 import AdminOrders from "./pages/AdminOrders";
 import AdminRequests from "./pages/AdminStyleRequest";
 import RequestPaymentPage from "./pages/RequestPaymentPage";
-
 import OrderHub from "./pages/OrderHub";
 import Shops from "./pages/Shops";
-
 
 function App() {
   return (
     <CartProvider>
       <Router>
 
-
-        {/* 🔥 GLOBAL LAYOUT WRAPPER */}
+        {/* 🔥 GLOBAL FULLSCREEN LAYOUT (FIXED) */}
         <div
-          className="min-h-screen flex flex-col bg-white dark:bg-black"
+          className="min-h-screen flex flex-col bg-slate-50 dark:bg-black"
           style={{
             paddingTop: "env(safe-area-inset-top)",
             paddingBottom: "env(safe-area-inset-bottom)",
           }}
-        ></div>
+        >
 
-        <ButtonBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/business" element={<Business />} />
-          <Route path="/admin" element={<Admin />} />
-          {/* <Route path="/business/dashboard" element={<BusinessDashboard />} /> */}
-          {/* 🏪 VENDOR DASHBOARD */}
-          <Route path="/vendor/:id" element={<Product />} />
-          <Route path="/tailor/:id" element={<Style />} />
+          {/* MAIN CONTENT */}
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/business" element={<Business />} />
+              <Route path="/admin" element={<Admin />} />
 
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/style/:id" element={<StyleDetails />} />
+              <Route path="/vendor/:id" element={<Product />} />
+              <Route path="/tailor/:id" element={<Style />} />
 
-          <Route
-            path="/dashboard/business/:id"
-            element={<BusinessDashboard />}
-          />
+              <Route path="/product/:id" element={<ProductDetails />} />
+              <Route path="/style/:id" element={<StyleDetails />} />
 
-<Route path="/orders-hub" element={<OrderHub />} />
+              <Route
+                path="/dashboard/business/:id"
+                element={<BusinessDashboard />}
+              />
 
-<Route path="/shop" element={<Shops />} />
+              <Route path="/orders-hub" element={<OrderHub />} />
+              <Route path="/shop" element={<Shops />} />
+              <Route path="/chat" element={<ChatPage />} />
 
-          <Route path="/chat" element={<ChatPage />} />
+              <Route
+                path="/business/:businessId/public"
+                element={<PublicSeller />}
+              />
 
+              <Route path="/order" element={<OrderPage />} />
+              <Route path="/admin/accounts" element={<AdminAccounts />} />
+              <Route path="/admin/orders" element={<AdminOrders />} />
+              <Route path="/admin/styles-request" element={<AdminRequests />} />
+              <Route path="/request-payment" element={<RequestPaymentPage />} />
+            </Routes>
+          </div>
 
-          <Route path="/business/:businessId/public" element={<PublicSeller />} />
+          {/* 🔥 FIXED BOTTOM NAV (SAFE AREA FRIENDLY) */}
+          <div className="sticky bottom-0 z-50">
+            <ButtonBar />
+          </div>
 
-          <Route path="/order" element={<OrderPage />} />
-          <Route path="/admin/accounts" element={<AdminAccounts />} />
-
-
-          <Route path="/admin/orders" element={<AdminOrders />} />
-          <Route path="/admin/styles-request" element={<AdminRequests />} />
-          <Route path="/request-payment" element={<RequestPaymentPage />} />
-
-        </Routes>
+        </div>
       </Router>
     </CartProvider>
   );
